0c07439560325067f0038326c1d7427f7e72e443
ÖZGE ODABAŞ <180315051@ogr.cbu.edu.tr>
2021-02-05 19:14:27 +0300

    .

--- Consolee/Program.cs
+++ Consolee/Program.cs
@@ -1,20 +1,10 @@
-﻿using Business.Concrete;
-using DataAccess.Concrete.InMemory;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace ConsoleUI
 {
-    class Program
+    class program
     {
-        static void Main(string[] args)
-        {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
-            foreach (var car in carManager.GetAll())
-            {
-                Console.WriteLine(car.Description);
-            }
-
-
-        }
     }
 }
